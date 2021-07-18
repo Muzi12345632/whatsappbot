d1496c6a70dfa63b981229ba77fc1cab502960f7
Muzi12345632 <84638389+Muzi12345632@users.noreply.github.com>
2021-07-18 23:09:52 +0200

    Add files via upload

---
+++ whatsapp response.py
@@ -0,0 +1,9 @@
+def response(input_message):
+    message = input_message.lower
+
+    if message == 'hie':
+        return 'salibonani'
+    elif message == 'good morning':
+        return 'uvuke njani'
+    else:
+        return 'not available'
